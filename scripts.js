94fa3bd210a3fb12701a0634d174ca1e9e12fb54
Lmickk2 <lukemickan21@icloud.com>
2022-11-03 02:55:14 -0400

    HTML file is mostly finished, along with some CSS. Questions have been added in JS

---
+++ scripts.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+        title: "Which built-in method returns the length of the string?",
+        choices: ["Length()", "Size()", "Index()", "None of the Above"],
+        answer: "Length()"
+    },
+    {
+        title: "Which of the following function of Number object returns a string value version of the current number?",
+        choices: ["toString()", "toFixed()", "toLocalString" , "toPrecision"],
+        answer: "toString()"
+    },
+    {
+        title: "A useful tool offered by Google Chrome is known as what?",
+        choices: ["GoogleDev", "Chrome Dev Tools", "iDevelop", "ProDevelop"],
+        answer: "Chrome Dev Tools"
+    },
+    {
+        title: "How do you declare a JavaScript variable x?",
+        choices: ["Define x", "def x", "var x", "variable x"],
+        answer: "var x"
+    },
+    {
+        title: "When we don’t assign a value to a variable it will be?",
+        choices: ["none", "NaN", "null", "undefined"],
+        answer: "undefined"
+    },
+
+    {
+        title: "To add an element to the end of an array you use:",
+        choices: ["pop()", "push()", "addTo()", "add()"],
+        answer: "push()"
+    }
+
+];
+
